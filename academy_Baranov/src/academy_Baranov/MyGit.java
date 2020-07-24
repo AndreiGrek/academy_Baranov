59b7f068cc7caf88ddffdf7f0bd4ed0de9035d4d
Andrei Baranov <andrei91077@gmail.com>
2020-07-24 20:38:36 +0300

    /

--- academy_Baranov/src/academy_Baranov/MyGit.java
+++
@@ -1,7 +0,0 @@
-package academy_Baranov;
-
-public class MyGit {
-	public static void main (String [] Args) {
-		System.out.println ("My first");
-	}
-}
--- academy_Baranov/src/academy_Baranov/Task1.java
+++
@@ -1,8 +0,0 @@
-package academy_Baranov;
-// Русский текст
-public class Task1 {
-	public static void main (String [] Args) {
-		System.out.println ("Русский текст!");
-	}
-}
-				
